c8e136427a1a32f00634cd496887d002c8cdad33
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2024-07-30 17:19:48 -0300

    Add files via upload

---
+++ perfilJS.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imagemField = document.getElementById('imagemField');
+    const imagemPreview = document.getElementById('imagemPreview');
+    const imgPerfilElements = document.querySelectorAll('.imgPerfil');
+    const saveButton = document.getElementById('saveButton');
+
+
+    const usuario = document.getElementById('usuario')
+    const savedValue = localStorage.getItem("inputValue")
+    if (savedValue) {
+        usuario.value = savedValue
+    }
+    usuario.addEventListener("input", function() {
+        localStorage.setItem("inputValue", usuario.value)
+    })
+
+    const sobre = document.getElementById('sobre')
+    const savedValue2 = localStorage.getItem("textAreaValue")
+    if (savedValue2) {
+        sobre.value = savedValue2
+    }
+    sobre.addEventListener("input", function() {
+        localStorage.setItem("textAreaValue", sobre.value)
+    })
+
+    const loadImagem = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const fileReader = new FileReader();
+            fileReader.onload = () => {
+                const dataUrl = fileReader.result;
+                imagemPreview.src = dataUrl;
+                imgPerfilElements.forEach(img => {
+                    img.src = dataUrl;
+                });
+            };
+            fileReader.readAsDataURL(file);
+        }
+    };
+
+    imagemField.addEventListener('change', loadImagem);
+
+    const savedImage = localStorage.getItem('editPerfil');
+    if (savedImage) {
+        imagemPreview.src = savedImage;
+        imgPerfilElements.forEach(img => {
+            img.src = savedImage;
+        });
+    }
+
+    saveButton.addEventListener('click', () => {
+        const contentPerfil = imagemPreview.src;
+        localStorage.setItem('editPerfil', contentPerfil);
+    });
+});
